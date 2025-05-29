2070881be249aef0470945a6fe1e7ca6aa665147
Yuya Saito <wasd132121@gmail.com>
2025-05-29 11:20:32 +0900

    refactor: コンストラクタでモデルインスタンスを作成し、各メソッドでモデルインスタンスを操作

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -7,11 +7,17 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    private $todo;
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
     // 一覧画面表示
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        $todos = $this->todo->all();
 
         return view('todo.index', ['todos' => $todos]);
     }
@@ -27,9 +33,8 @@ class TodoController extends Controller
     {
         $inputs = $request->all();
 
-        $todo = new Todo(); 
-        $todo->fill($inputs);
-        $todo->save();
+        $this->todo->fill($inputs);
+        $this->todo->save();
 
         return redirect()->route('todo.index');
     }
@@ -37,9 +42,7 @@ class TodoController extends Controller
     // 変更画面表示
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
-
+        $todo = $this->todo->find($id);
         return view('todo.show', ['todo' => $todo]);
     }
 }
